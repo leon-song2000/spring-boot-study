cb5d0344d8b3f423ca09f71b2ae068054de0fb0d
songlin01 <songlin01@baidu.com>
2017-11-07 13:03:31 +0800

    spring-boot-quickstart

---
+++ quickstart/src/main/java/com/leon/App.java
@@ -0,0 +1,16 @@
+package com.leon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        String[] beanNames = context.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
